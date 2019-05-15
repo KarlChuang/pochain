705844c920c4e6be0577ae7d47440e7dfa6800e7
KarlChuang <b05901043@ntu.edu.tw>
2019-05-15 23:51:27 +0800

    add picture in propose

--- src/Components/Propose.js
+++ src/Components/Propose.js
@@ -12,19 +12,60 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const ProposeBlock = styled.div`
-  width: 80%;
+const ImgProposeBlock = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+const ImgInputBox = styled.div`
+  width: 300px;
+  height: 300px;
+  margin: 10px;
+  margin-left: 50px;
+  background-color: #c5c5c51c;
+  border: 5px solid #5f809054;
+  transition: .3s;
+  :hover {
+    background-color: #5b7c8c9e;
+    border: 5px solid #5f809000;
+    transition: .3s;
+  }
+`;
+
+const ImgForm = styled.form`
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const ImgInput = styled.input`
+  width: 300px;
+  height: 300px;
+  display: inline-block;
+  cursor: pointer;
+  opacity: 0;
 `;
 
 const Img = styled.img`
+  height: 300px;
+  margin: 10px;
+  min-height: 300px;
+`;
+
+const Icon = styled.i`
+  font-size: 120px;
+  position: absolute;;
+  margin: auto;
+  color: #dde2e4;
 `;
 
 class Propose extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      file: null,
-      imagePreviewUrl: '',
+      files: [],
+      imagePreviewUrls: [],
     };
     this.handleInputChange = this.handleInputChange.bind(this);
   }
@@ -32,29 +73,28 @@ class Propose extends Component {
     e.preventDefault();
     const reader = new FileReader();
     const file = e.target.files[0];
-    console.log('hi');
     reader.onloadend = () => {
       this.setState({
-        file,
-        imagePreviewUrl: reader.result,
+        files: [...this.state.files, file],
+        imagePreviewUrls: [...this.state.imagePreviewUrls, reader.result],
       });
     };
     reader.readAsDataURL(file);
   }
   render() {
-    // const ImgTag = ((this.state.imgPath === '') ? (
-    //   <input type="file" onChange={e => this.setState({ imgPath: e.target.value })} />
-    // ) : (
-    //   <img src={this.state.imgPath} alt="hi" />
-    // ));
     return (
       <Wrapper>
-        <ProposeBlock>
-          <form>
-            <input name="Product_img" type="file" accept="image/gif, image/jpeg, image/png" onChange={this.handleInputChange} />
-          </form>
-          <img src={this.state.imagePreviewUrl} alt="hi" />
-        </ProposeBlock>
+        <ImgProposeBlock>
+          <ImgInputBox>
+            <ImgForm>
+              <Icon className="fas fa-camera" />
+              <ImgInput name="Product_img" type="file" accept="image/gif, image/jpeg, image/png" onChange={this.handleInputChange} />
+            </ImgForm>
+          </ImgInputBox>
+          {
+            this.state.imagePreviewUrls.map(url => (<Img src={url} key={url} alt={url} />))
+          }
+        </ImgProposeBlock>
       </Wrapper>
     );
   }
